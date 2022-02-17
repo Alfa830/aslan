2a55ea246bfc7cf514e639e8b0c3c7e5ed1fde68
alfa <aleascoetti@gmail.com>
2022-02-17 20:05:01 -0300

    Primer Commit

---
+++ src/ejercicio12/Ejercicio12.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ALFA830
+ */
+public class Ejercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Escriba un programa que valide si una nota está entre 0 y 10, sino está entre 0 y 10
+        //la nota se pedirá de nuevo hasta que la nota sea correcta. 
+        
+        int num;
+        Scanner leer = new Scanner(System.in);
+        
+        do{
+            System.out.println("Ingrese un numero del 0 al 10 ");
+            num =leer.nextInt(); 
+            
+           
+        }while (num >= 0 & 10 < num );
+        
+    }
+    
+}
